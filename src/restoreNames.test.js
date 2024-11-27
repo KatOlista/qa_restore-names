ce2be7224eff85b3c4b02103afb9f24ccfd35edb
KatOlista^^! <olya.katyushina@gmail.com>
2024-11-27 12:06:08 +0100

    add tests

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,47 @@
 'use strict';
 
+const users = [
+  {
+    firstName: undefined,
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+  },
+  {
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  },
+];
+
+const expectedResult = [
+  {
+    firstName: 'Jack',
+    lastName: 'Holy',
+    fullName: 'Jack Holy',
+  },
+  {
+    firstName: 'Mike',
+    lastName: 'Adams',
+    fullName: 'Mike Adams',
+  },
+];
+
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
-
+  it('should be declared', () => {
+    expect(restoreNames)
+      .toBeInstanceOf(Function);
   });
 
-  // write tests here
+  it(`should add 'firstName' if it is undefined`, () => {
+    restoreNames(users);
+
+    expect(users).toEqual(expectedResult);
+  });
+
+  it(`should not add 'firstName' if it is defined`, () => {
+    restoreNames(users);
+
+    expect(users).toEqual(expectedResult);
+  });
 });
